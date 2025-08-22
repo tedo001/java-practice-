bcf043c29c60c63fd3c43b736a8d1aca708a0d58
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-08-22 14:50:58 +0530

    Create POLY.java
    for class pratice

---
+++ POLY.java
@@ -0,0 +1,24 @@
+public class Main {
+    private int age ;
+    private String name ;
+    
+    public  void setAge(int NewAge ){
+        age = NewAge;
+        }
+    public  void setName(String NewName){
+        name = NewName;
+    } 
+    public String getName(){
+     return name;
+   }
+    public int getAge(){
+     return age;
+   }
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.setName("abi");
+        m.setAge(10);
+        System.out.println("Name:"+m.getName());
+        System.out.println("Age:"+m.getAge());
+    }
+}
