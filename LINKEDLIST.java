0d7fcb7299cb0d193ec57c35d84902d52b7d7679
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-08-25 11:24:00 +0530

    Update LINKEDLIST.java

--- LINKEDLIST.java
+++ LINKEDLIST.java
@@ -1,38 +1,60 @@
 import java.util.Scanner;
+
 class Node {
-	int data ;
-	Node next;
-	Node(int data) {
-		this.data=data;
-		this.next= null;
-	}
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
-public class Main
-{
-	static Node head= null;
-	static void append(int data) {
-		Node newNode=new Node(data);
-		if(head==null) {
-			head=newNode;
-		}
-		else {
-			Node temp = head;
-		}	
-		while(temp.next!=null) {
-				System.out.println(temp.data+"");
-				temp=temp.next;
-			}
-	}
-	public static void main(String[] args) {
-		scanner sc = new scanner(System.in);
-		System.out.println("enter no of nodes:");
-		int n= sc.nextInt();
-		for(int i=0; i<n; i++) {
-			int data=sc.nextint();
-			if(data>0) {
-				append(data);
-			}
-		}
-		display();
-	}
+
+public class Main {
+    static Node head = null;
+
+    // Method to append a node to the end of the list
+    static void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    // Method to display the linked list
+    static void display() {
+        Node temp = head;
+        System.out.print("Linked List: ");
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter data for node " + (i + 1) + ": ");
+            int data = sc.nextInt();
+            if (data > 0) {
+                append(data);
+            } else {
+                System.out.println("Only positive integers are allowed.");
+            }
+        }
+
+        display();
+        sc.close(); // good practice to close the Scanner
+    }
 }
