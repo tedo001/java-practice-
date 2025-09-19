fdf7e60ef4f15bc80595b0380834020f0b2a812f
durgamani sasikumar <durgamani.d.e.c.e.50@gmail.com>
2025-09-19 16:23:09 +0530

    Create dijkstra.java
    incomplete code

---
+++ dijkstra.java
@@ -0,0 +1,34 @@
+ie;
+
+    for(int v-0;v<v;v++){
+      if(!visited[]v) && graph[u][v] !=0 && dist[u] != integer.maxvale && dist[u] + graph[u][v] < dist[v])
+        {
+        dist[v]= dist[v] + graph[umport java.util.scanner;
+public class Main{
+ static final int v =3;
+
+int mindistance(int distp[],boolean visited[]){
+  int min = integer/max_value,minindex =-1;
+  for(int v=0;v<v;v++){
+    if(!visited[v] && dist[v]<= min){
+      min =dist[v];
+      min index= v;
+    }
+  }
+return minIndex
+  }
+  void dijkstra(int graph[][].int src){
+
+  int dist[]=new int[v];
+    boolean visited[] = new boolean[v];
+
+  arrays.fill(dist,integer.max_valu);
+    dist[src];
+    for(int count =0lcount<v-1; count++){
+      int u = mindistance(dist,visited);
+      visited[u]=tru][v];
+      }
+    }
+    }
+    systen=m.out.print("vertex \t distance from source");
+    
