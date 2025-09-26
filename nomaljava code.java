7ba60b11fba3e88d8184fecbaee1014e55724bd9
durgamani sasikumar <durgamani.d.e.c.e.50@gmail.com>
2025-09-26 15:04:13 +0530

    Create nomaljava code.java

---
+++ nomaljava code.java
@@ -0,0 +1,18 @@
+public class NormalCode {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    static void task1() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Task 1 - Step " + i);
+        }
+    }
+
+    static void task2() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Task 2 - Step " + i);
+        }
+    }
+}
