dfd6ed6572276058c02f10de5334de16dd8caa13
durgamani sasikumar <durgamani.d.e.c.e.50@gmail.com>
2025-10-07 11:24:18 +0530

    Create TRAINIG..java

---
+++ TRAINIG..java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int money = 500;
+
+        if (money < 300) {
+            System.out.println("not enough");
+        } else {
+            System.out.println("much better");
+        }
+    }
+}
