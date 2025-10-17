2ba2422158b429bab8d009fb0db6818e4af937f9
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-10-17 17:57:25 +0530

    nothing

---
+++ trainig by emc.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+public static void main(String[] args)
+Scanner scan = new scan(System.in);
+String name= scan.nextLine();
+int age = scan.nextint();
+System.out.println("name:"+mani);
+System.out.println("age:"+ 19);
+}
+}
