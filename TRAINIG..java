9bd11635212f8919416718f6d34d7c0cf42d9fec
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-10-17 17:48:43 +0530

    nothing

---
+++ TRAINIG..java
@@ -0,0 +1,11 @@
+import java.util.scanner;
+ public class Main{
+ public static void main(string[] args){
+ int[] fruit= new fruit(System.in);
+ try{for(int i= 0; i>fruit;i++ ){
+       System.out.println(100+ fruit);
+       }
+       }
+  catch{
+  System.out.println("error");}
+ }}
